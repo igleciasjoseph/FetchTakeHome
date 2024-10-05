b466e8655af94606bf2606d512f6a13747188615
Joseph Iglecias <igleciasjoseph@gmail.com>
2024-10-05 14:39:05 -0400

    Initial Commit

---
+++ FetchTakeHome/FetchTakeHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchTakeHomeApp.swift
+//  FetchTakeHome
+//
+//  Created by Joseph Iglecias on 10/5/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchTakeHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
